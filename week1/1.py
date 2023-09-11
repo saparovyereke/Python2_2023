7bd86985c5621fcdb0ed53118f7de6999644041e
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-09-12 01:47:27 +0600

    Add files via upload

---
+++ week1/1.py
@@ -0,0 +1,28 @@
+a = 10/3 #division = 3.333
+b = 10//3 #integer division = 3
+print(a," ",b)
+
+c = 10**3 #10 in power of 3 = 1000
+print(c)
+
+x=1
+x+=2 #it means x=x+2 so result will be 3
+print(x)
+
+print(float(1)) #number with decimals 1.0
+
+print(bool("False")) #True
+
+#falsy values are values that gives False in boolean or include empty sequences
+
+print(10 == "10") #False
+
+print("bag" > "apple") #True
+
+print(not(True or False)) #False
+
+#10.Under what circumstances does the expression 18 <= age < 65 evaluate to True?
+#If variable 'age' will be in range of {18 to 65]
+print(range(1, 10, 2))
+
+#Lists, tuples, dictionaries, and sets are all iterable objects.
---
+++ week1/2.py
@@ -0,0 +1,82 @@
+#1 type "python --version" in terminal
+
+#2 task
+# a = int(input())
+# b = 2*a
+# c = a*b
+# print(c)
+
+#3 task
+# firstname = input()
+# lastname = input()
+# print(lastname,firstname)
+
+#4 task
+# values = input("Input some comma seprated numbers : ")
+# list = values.split(",")
+# tuple = tuple(list)
+# print('List : ',list)
+# print('Tuple : ',tuple)
+
+#5 task
+# mylist = [1,2,3,4,5,6,7,8]
+# print(mylist[0],mylist[-1])
+
+# mylist = []
+# n = int(input())
+# for num in range(n):
+#     x = int(input())
+#     mylist.append(x)
+# print(mylist[0],mylist[-1])
+    
+#6 task
+# n = int(input())
+# print(n+n*n+n*n*n)
+
+# #7 task
+# radius = 8
+# volume = 4/3 * 3.141592653589793 * radius * radius * radius
+# print (volume)
+
+#8 task
+# n = int(input())
+# if (abs(1000-n) <= 100) or (abs(2000-n)<=100):
+#     print(True)
+# else:
+#     print(False)
+
+#9 task
+# thislist = [1, 5, 8, 3] 
+# n = int(input())
+# if n in thislist:
+#     print(True)
+# else:
+#     print(False)
+
+#10 task
+# b = int(input())
+# h = int(input())
+# a = 0.5 * b * h
+# print(a)
+
+#11 task
+# import math
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+
+# d=((x2-x1)**2)+((y2-y1)**2)
+# print(math.sqrt(d))
+
+#12 task
+# a, b, c, d, e, f = map(float, input().split())
+# n = a*e - b*d
+# print("Values of x and y:")
+# if n != 0:
+#     x = (c*e - b*f) / n
+#     y = (a*f - c*d) / n
+#     print('{:.3f} {:.3f}'.format(x+0, y+0))
+
+#13 task
+x1, y1, x2, y2, x3, y3 = map(float, input().split())
