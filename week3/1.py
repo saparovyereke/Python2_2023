e8706ead06809c96b21846664c42e722d1aa32df
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-09-19 16:40:37 +0600

    Add files via upload

---
+++ week3/1.py
@@ -0,0 +1,44 @@
+#1 task
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# for i in a:
+#     if(i<5):
+#         print(i)
+
+#2 task
+# a = input()
+# if (a[::-1]==a):
+#     print("String is palindrome")
+# else:
+#     print("String in not palindrome")
+
+#3 task
+# a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+# b = []
+# for i in a:
+#     if(i%2==0):
+#         b.append(i)
+# print(b)
+
+#4-5 task
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# c = {0}
+# for i in a:
+#     for j in b:
+#         if i==j:
+#             c.add(j)
+# print(c)            
+
+
+#6 task
+# mylist = []
+# newlist = []
+# n = int(input())
+# for num in range(n):
+#     x = int(input())
+#     mylist.append(x)
+# newlist.append(mylist[0])
+# newlist.append(mylist[-1])
+# print(newlist)
+
+
---
+++ week3/2.py
@@ -0,0 +1,6 @@
+# 1 task
+# mystring = input()
+# print(len(mystring))
+
+#2 task
+# import re
