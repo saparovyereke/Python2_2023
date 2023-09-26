56a6efb9f4de29d1420e0efd73769a8f442eac6b
saparovyereke <123382638+saparovyereke@users.noreply.github.com>
2023-09-26 12:53:16 +0600

    Add files via upload

--- week3/2.py
+++ week3/2.py
@@ -2,5 +2,49 @@
 # mystring = input()
 # print(len(mystring))
 
-#2 task
-# import re
+# 2 task
+# def count_characters(string):
+#     char_frequency = {}
+
+#     for char in string:
+#         if char in char_frequency:
+#             char_frequency[char] += 1
+#         else:
+#             char_frequency[char] = 1
+
+#     return char_frequency
+
+# sample_string = str(input())
+
+# result = count_characters(sample_string)
+# print(result)
+
+# 3 task
+# ourstring = str(input())
+# modified_string = ourstring[:2] + ourstring[-2:]
+# print(modified_string)
+
+#22 task
+# number = float(input())
+# formatted_number = f"{number:.2f}"
+# print(formatted_number)
+
+
+#5 task
+# def swap_and_combine_strings(str1, str2):
+#     if len(str1) >= 2 and len(str2) >= 2:
+#         new_str1 = str2[:2] + str1[2:]
+#         new_str2 = str1[:2] + str2[2:]
+        
+#         result = new_str1 + ' ' + new_str2
+#     else:
+#         result = str1 + ' ' + str2
+
+#     return result
+
+# string1 = str(input())
+# string2 = str(input())
+
+# result = swap_and_combine_strings(string1, string2)
+# print(result)
+
